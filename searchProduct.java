b4ee75956d8cdd62f7ef6843d61ca3b3dc166b7a
rrventure <98668776+rrventure@users.noreply.github.com>
2023-04-27 23:14:29 -0400

    Create searchProduct.java

---
+++ searchProduct.java
@@ -0,0 +1,17 @@
+package TutorialNinjaBootcamp;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SearchProduct {
+	@Test
+	void searchProductByName()
+	{
+	System.out.println("Search Product by Name");
+		Assert.assertEquals("rose", "rose");//true
+	}
+	
+	
+	
+
+}
