e60a7071e91a0a5192932b20652ab5aa29c79c4d
rrventure <98668776+rrventure@users.noreply.github.com>
2023-04-27 23:12:59 -0400

    register

---
+++ register.java
@@ -0,0 +1,17 @@
+package TutorialNinjaBootcamp;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Register {
+
+	@Test
+	void registerByEmail()
+	{
+		System.out.println("Register by Email");
+		Assert.assertEquals("rose", "rose");//true
+	}
+	
+	
+	
+}
